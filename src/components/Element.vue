c63f9db151c83bfbb8bdc6efebe88c2489975271
Arthur Suslov <artursuslov@gmail.com>
2022-09-20 15:40:01 +0300

    first commit

---
+++ src/components/Element.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="element">
+    <h1 class="">{{ json.name }}</h1>
+    <h2 class="">{{ json.age }}</h2>
+  </div>
+</template>
+<script setup lang="ts">
+interface Test {
+  name: string;
+  age: number;
+}
+const props = defineProps<{
+  json: Test
+}>()
+</script>
+<style lang="scss" scoped>
+
+</style>
